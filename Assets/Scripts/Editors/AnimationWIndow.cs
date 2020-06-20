1a9f5c04ab68722cdf48e44bc53e1bad81a336ea
Alexander <aposi324@live.kutztown.edu>
2020-06-19 22:46:54 -0400

    Start of custom animation window

--- Assets/Scripts/Editors/AnimationWIndow.cs
+++ Assets/Scripts/Editors/AnimationWIndow.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 using UnityEditor;
 public class AnimationWindow : EditorWindow
 {
+    private GameObject selectedObject;
+   // private AnimationClip clip = new AnimationClip();
     [MenuItem("Window/Animation Window")]
+    
     public static void ShowWindow()
     {
         EditorWindow.GetWindow(typeof(AnimationWindow));
     }
+
+  
+  
     private void OnGUI()
     {
         // Window code goes here
+        //selectedObject = Selection.activeTransform.gameObject;
+        //Debug.Log(selectedObject.name);
+        GUILayout.BeginHorizontal();
+            GUILayout.BeginVertical();
+        int selected = 0;
+        string[] options = new string[] { "Stand", "Fall", "Fair" };
+        EditorGUILayout.Popup("Animation", selected, options);
+                GUILayout.Box("1");
+            GUILayout.EndVertical();
+            
+            GUILayout.BeginVertical();
+                GUILayout.Box("2");
+                GUILayout.Box("3");
+        GUILayout.EndHorizontal();
     }
 }
